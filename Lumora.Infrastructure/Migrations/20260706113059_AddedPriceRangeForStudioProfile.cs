6a89d668f33cface29a1a0c4823f852a139d044e
Amogh Thampuran <amogh.thampuran@qburst.com>
2026-07-06 17:02:36 +0530

    Updated studio information.

---
+++ Lumora.Infrastructure/Migrations/20260706113059_AddedPriceRangeForStudioProfile.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lumora.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class AddedPriceRangeForStudioProfile : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<decimal>(
+            name: "MaxPrice",
+            table: "StudioProfiles",
+            type: "numeric(18,2)",
+            precision: 18,
+            scale: 2,
+            nullable: false,
+            defaultValue: 0m);
+
+        migrationBuilder.AddColumn<decimal>(
+            name: "MinPrice",
+            table: "StudioProfiles",
+            type: "numeric(18,2)",
+            precision: 18,
+            scale: 2,
+            nullable: false,
+            defaultValue: 0m);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "MaxPrice",
+            table: "StudioProfiles");
+
+        migrationBuilder.DropColumn(
+            name: "MinPrice",
+            table: "StudioProfiles");
+    }
+}
