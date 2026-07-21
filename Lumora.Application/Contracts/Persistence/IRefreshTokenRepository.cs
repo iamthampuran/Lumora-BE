4634061d1aa3ba3a8799f91e2dafaa0a5f030573
Amogh Thampuran <amogh.thampuran@qburst.com>
2026-07-21 12:32:56 +0530

    Implemented a sample refreshtoken repository.

---
+++ Lumora.Application/Contracts/Persistence/IRefreshTokenRepository.cs
@@ -0,0 +1,17 @@
+﻿using Lumora.Domain.Entities.Identity;
+using System.Linq.Expressions;
+
+namespace Lumora.Application.Contracts.Persistence;
+
+public interface IRefreshTokenRepository
+{
+    Task<List<RefreshToken>> GetAsync(
+        Expression<Func<RefreshToken, bool>> predicate,
+        CancellationToken cancellationToken = default);
+
+    Task<RefreshToken?> GetFirstAsync(
+        Expression<Func<RefreshToken, bool>> predicate,
+        CancellationToken cancellationToken = default);
+
+    void Add(RefreshToken refreshToken);
+}
