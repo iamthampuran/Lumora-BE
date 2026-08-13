6c98d1990cfdedbb8486ab20f8a545e635f1593e
Amogh Thampuran <amogh.thampuran@qburst.com>
2026-08-13 12:00:17 +0530

    Returns presignedUrl instead of actual url.

--- Lumora.Application/Contracts/Services/IAuthService.cs
+++ Lumora.Application/Contracts/Services/IAuthService.cs
@@ -6,7 +6,7 @@ public interface IAuthService
 {
     (string passwordHash, byte[] salt) HashPasswordAsync(string passwordPlainText);
     bool VerifyPasswordAsync(string passwordPlainText, string hashedPassword, byte[] salt);
-    string GenerateAccessTokenAsync(User user);
+    Task<string> GenerateAccessTokenAsync(User user);
     (string refreshToken, RefreshToken entity) GenerateRefreshTokenAsync(User user);
     Task<User?> ValidateRefreshTokenAsync(string token, CancellationToken cancellationToken);
     Task<bool> RevokeRefreshTokenAsync(string token, CancellationToken cancellationToken);
