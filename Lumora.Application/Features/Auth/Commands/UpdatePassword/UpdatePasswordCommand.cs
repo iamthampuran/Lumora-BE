8d9f7f57afe88c197b7b9fd1c3a97c09280860b5
Amogh Thampuran <amogh.thampuran@qburst.com>
2026-09-17 14:25:54 +0530

    Implemented update password functionality.

---
+++ Lumora.Application/Features/Auth/Commands/UpdatePassword/UpdatePasswordCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace Lumora.Application.Features.Auth.Commands.UpdatePassword;
+
+public record UpdatePasswordCommand(string NewPassword, string CurrentPassword);
---
+++ Lumora.Application/Features/Auth/Commands/UpdatePassword/UpdatePasswordHandler.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.Result;
+using Lumora.Application.Contracts.Common;
+using Lumora.Application.Contracts.Persistence;
+using Lumora.Application.Contracts.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Lumora.Application.Features.Auth.Commands.UpdatePassword;
+
+public class UpdatePasswordHandler(ILogger<UpdatePasswordCommand> logger, IUserRepository userRepository, IUnitOfWork unitOfWork, IAuthService authService)
+{
+    public async Task<Result<Guid>> Handle(UpdatePasswordCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling command - {@command}", nameof(UpdatePasswordCommand));
+        var userDetails = unitOfWork.GetCurrentUserDetails();
+        if (userDetails == null) return Result.Unauthorized("User is not logged in");
+
+        var user = await userRepository.GetByIdAsync(userDetails.UserId, cancellationToken);
+        if (user == null) return Result.NotFound("User is not found");
+
+        var isCorrectPassword = authService.VerifyPasswordAsync(command.CurrentPassword, user.PasswordHash, user.Salt);
+        if (!isCorrectPassword)
+            return Result.Error("The entered current password is wrong");
+
+
+        var newPasswordDetails = authService.HashPasswordAsync(command.NewPassword);
+        user.PasswordHash = newPasswordDetails.passwordHash;
+        user.Salt = newPasswordDetails.salt;
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return Result.Success(user.Id);
+    }
+}
