9dd393178317115703e4bfe99e1c8d3a022b3d51
Amogh Thampuran <amogh.thampuran@qburst.com>
2026-09-18 17:16:11 +0530

    Implemented studio inquiry list endpoint

---
+++ Lumora.Application/Features/Studio/Queries/GetInquiries/GetInquiriesQuery.cs
@@ -0,0 +1,15 @@
+﻿using Lumora.Application.Helpers;
+using Lumora.Domain.Enums;
+
+namespace Lumora.Application.Features.Studio.Queries.GetInquiries;
+
+public record GetInquiriesQuery(InquiryStatus Status, PaginationOptions PaginationOptions, InquiryFilterOptions? FilterOptions);
+
+public record InquiryFilterOptions(
+    IEnumerable<Guid>? EventTypeIds,
+    DateOnly? StartDate,
+    DateOnly? EndDate,
+    string? Location,
+    decimal? MinAmount,
+    decimal? MaxAmount
+);
